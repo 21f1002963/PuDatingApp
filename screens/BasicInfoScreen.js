5909171adb60caa0eddcf74ca6d6512cd365b627
21F1002963 <21f1002963@ds.study.iitm.ac.in>
2025-03-02 21:32:58 +0530

    2 pages done

--- screens/BasicInfoScreen.js
+++ screens/BasicInfoScreen.js
@@ -1,11 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Platform, Pressable } from 'react-native'
 import React from 'react'
+import LottieView from 'lottie-react-native'
+import { useNavigation } from '@react-navigation/native'
 
 const BasicInfo = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>BasicInfo</Text>
-    </View>
+    <SafeAreaView style={{ paddingTop: Platform.OS === 'android' ? 200 : 0 , flex:1, backgroundColor: 'white'}}>
+      <View>
+        <Text style={{
+          fontSize: 32,
+          fontWeight: 'bold',
+          fontFamily: 'GeezaPro-Bold',
+          marginLeft: 20,
+        }}>You're one of a kind</Text>
+        <Text style={{
+          fontSize: 32,
+          fontWeight: 'bold',
+          fontFamily: 'GeezaPro-Bold',
+          marginTop: 10,
+          marginLeft: 20,
+        }}>Your profile should be too.</Text>
+      </View>
+      <View>
+        <LottieView
+        style={{
+          height: 260,
+          width: 300,
+          alignSelf: 'center',
+          marginTop: 40,
+          justifyContent: 'center',
+        }}
+        source= {require('../assets/love.json')}
+        autoPlay
+        loop={true}
+        speed={0.7}
+        />
+      </View>
+      <Pressable 
+      style={{ backgroundColor: '#900C3F', justifyContent: 'center', alignItems: 'center', marginTop: 'auto', padding: 15}}
+      onPress={() => navigation.navigate('Name')}>
+        <Text style={{
+          color: 'white',
+          fontSize: 18,
+          fontWeight: '600',
+          fontFamily: 'GeezaPro',
+
+        }}>Enter Basic Info</Text>
+      </Pressable>
+    </SafeAreaView>
   )
 }
 
--- screens/NameScreen.js
+++ screens/NameScreen.js
@@ -1,11 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
+import Ionicons from '@react-native-vector-icons/ionicons'
+import { Image } from 'react-native'
 
 const NameScreen = () => {
+  const [firstName, setFirstName] = React.useState('')
   return (
-    <View>
-      <Text>NameScreen</Text>
-    </View>
+    <SafeAreaView
+      style={{ paddingTop: Platform.OS === 'android' ? 50 : 0, flex: 1, backgroundColor: 'white' }}>
+
+      <Text style={{ marginTop: 50, textAlign: 'center', color: 'gray' }}
+      >NO BACKGROUND CHECKS ARE CONDUCTED</Text>
+
+      <View style={{ marginTop: 30, marginHorizontal: 20 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View style={{ width: 44, height: 44, borderRadius: 22, borderWidth: 2, borderColor: 'black', justifyContent: 'center', alignItems: 'center' }}>
+            <Ionicons name="newspaper-outline" size={26} color="black" />
+          </View>
+          <Image style={{ width: 100, height: 40 }}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png'
+            }}>
+          </Image>
+        </View>
+        <View style={{ marginTop: 30 }}>
+          <Text style={{
+            fontSize: 25, fontWeight: 'bold', fontFamily: 'GeezaPro-Bold'
+          }}> What's your name?
+          </Text>
+          <TextInput
+          value={firstName} 
+          onChangeText={ text => setFirstName(text)}
+          autoFocus={true} placeholder='First Name (required)'
+          placeholderTextColor={"#BEBEBE"}
+          style={{
+            width:340,
+            // marginVertical: 10,
+            marginTop: 25,
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            paddingBottom: 10,
+            fontFamily: 'GeezaPro-Bold',
+            fontSize: firstName ? 22 : 22
+          }}/> 
+          <TextInput
+          placeholder='Last Name'
+          placeholderTextColor={"#BEBEBE"}
+          style={{
+            width:340,
+            // marginVertical: 10,
+            marginTop: 25,
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            paddingBottom: 10,
+            fontFamily: 'GeezaPro-Bold',
+            fontSize: firstName ? 22 : 22
+          }}/>
+          <Text style={{marginTop: 8 }}>Last name is optional</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
